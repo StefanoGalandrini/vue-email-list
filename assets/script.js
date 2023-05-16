7babd9c6fc03e47bee3d5b28de60dfbf8811661e
StefanoGalandrini <stefe68@gmail.com>
2023-05-16 17:33:57 +0200

    Add Timeout function to get 10 different emails

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,34 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			
-		},
+			randomEmail: "",
+			emailArray: [],
+		};
 	},
 
 	methods: {
-		
+		getRandomEmail(n) {
+			this.emailArray.length = 0;
+			for (i = 0; i < n; i++) {
+				setTimeout(() => {
+					axios
+						.get(
+							"https://flynn.boolean.careers/exercises/api/random/mail",
+						)
+						.then((email) => {
+							this.randomEmail = email.data.response;
+							this.emailArray.push(this.randomEmail);
+						});
+				}, 200);
+			}
+		},
 	},
 
+	created() {
+		axios
+			.get("https://flynn.boolean.careers/exercises/api/random/mail")
+			.then((email) => (this.randomEmail = email.data.response));
+	},
 });
 
 app.mount("#container");
